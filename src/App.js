ccfc05fb7ad8b8b22f945e92c4016db367787b83
Thanh Le <thanhlevan0203@gmail.com>
2022-08-09 22:59:47 -0700

    Installed Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Register from './Register';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Register />
     </div>
   );
 }
--- src/Register.js
+++ src/Register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRef, useState, useEffect } from 'react' 
 
 const Register = () => {
   return (
